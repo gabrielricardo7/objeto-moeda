2efb146ef331177180c3b7a63939538e10d0a11a
Gabriel Ricardo <gabrielricardo7@gmail.com>
2021-10-14 23:05:10 -0300

    Add html semantic elements and settimeout to main functions

--- coinObject.js
+++ coinObject.js
@@ -27,13 +27,20 @@ const coin = {
     },
 };
 
+displayHeader();
+const main = document.createElement("main");
+document.body.appendChild(main);
+displayFooter();
+
 function display20Flips() {
     const results = [];
     for (let i = 0; i < 20; i++) {
-        results.push(coin.flip());
-        const text = document.createElement("p");
-        text.innerText = coin.toString();
-        document.body.appendChild(text);
+        setTimeout(function timer() {
+            results.push(coin.flip());
+            const text = document.createElement("p");
+            text.innerText = coin.toString();
+            main.appendChild(text);
+        }, i * 1000);
     }
     return results;
 }
@@ -43,7 +50,7 @@ function display20Images() {
     for (let i = 0; i < 20; i++) {
         setTimeout(function timer() {
             results.push(coin.flip());
-            document.body.appendChild(coin.toHTML());
+            main.appendChild(coin.toHTML());
         }, i * 1000);
     }
     return results;
@@ -53,12 +60,58 @@ function displayOneFlip() {
     let result = coin.flip();
     const text = document.createElement("p");
     text.innerText = coin.toString();
-    document.body.appendChild(text);
+    main.appendChild(text);
     return result;
 }
 
 function displayOneImage() {
     let result = coin.flip();
-    document.body.appendChild(coin.toHTML());
+    main.appendChild(coin.toHTML());
     return result;
+}
+
+function displayHeader() {
+    const header = document.createElement("header");
+    const h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+    const btnOneFlip = document.createElement("button");
+    const btnOneImage = document.createElement("button");
+    const btn20Flips = document.createElement("button");
+    const btn20Images = document.createElement("button");
+    const btnReset = document.createElement("button");
+
+    h1.textContent = "Heads/Tails";
+    h2.textContent = "Select a option:";
+    btnOneFlip.textContent = "Flip a coin (text)";
+    btnOneImage.textContent = "Flip a coin (image)";
+    btn20Flips.textContent = "Flip 20 coins (text)";
+    btn20Images.textContent = "Flip 20 coins (image)";
+    btnReset.textContent = "Reset";
+
+    btnOneFlip.addEventListener("click", displayOneFlip);
+    btnOneImage.addEventListener("click", displayOneImage);
+    btn20Flips.addEventListener("click", display20Flips);
+    btn20Images.addEventListener("click", display20Images);
+    btnReset.addEventListener("click", resetMain);
+
+    header.appendChild(h1);
+    header.appendChild(h2);
+    header.appendChild(btnOneFlip);
+    header.appendChild(btnOneImage);
+    header.appendChild(btn20Flips);
+    header.appendChild(btn20Images);
+    header.appendChild(btnReset);
+    document.body.appendChild(header);
+}
+
+function resetMain() {
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+}
+
+function displayFooter() {
+    const footer = document.createElement("footer");
+    footer.innerHTML = "<p>&copy; 2021 Gabriel Ricardo (Q1 JULHO-21) at Kenzie Academy Brazil</p>";
+    document.body.appendChild(footer);
 }
